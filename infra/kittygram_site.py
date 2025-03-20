a427ebc7f7ca8527dc64d02660d7c87723623d35
Susbring <zuyev2001@yandex.ru>
2025-03-20 03:31:13 +0000

    New in

--- infra/kittygram_site.py
+++ infra/kittygram_site.py
@@ -1,5 +1,5 @@
 IP: 158.160.90.193;
-name_taski: https://taskibackpit.ddns.net;
+name_taski: полная ссылка (https://доменное_имя) на ваш проект Taski;
 name_kittygram: https://kittygramyapr.ddns.net;
 login: admin;
 password: Uffhfuffhf1;
